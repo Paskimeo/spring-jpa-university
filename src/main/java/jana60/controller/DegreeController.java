1498d4abb983eec3a43f424bc2202ce4db68a6e0
paskimeo <paskimeo@gmail.com>
2022-07-18 16:01:11 +0200

    aggiunto controller degree

---
+++ src/main/java/jana60/controller/DegreeController.java
@@ -0,0 +1,31 @@
+package jana60.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jana60.modello.Degree;
+import jana60.repository.DegreeRepository;
+
+
+@Controller
+@RequestMapping("/degrees")
+public class DegreeController {
+	
+	@Autowired
+	private DegreeRepository repo;
+	
+	
+	@GetMapping
+	public String degrees(Model model)
+	{
+		List<Degree> degList = (List<Degree>) repo.findAll();
+		model.addAttribute("degList", degList);
+		return "degrees";
+	}
+
+}
